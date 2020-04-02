3a8b85b7c0ae34080d6e8d2402fb28c31ab68c68
Kamil Milik <kamilmilik@gmail.com>
2020-04-02 21:05:50 +0200

    Backend:  -Refactor

--- src/main/java/com/example/masterdegree/models/mappers/CriteriaMapper.java
+++ src/main/java/com/example/masterdegree/models/mappers/CriteriaMapper.java
@@ -20,27 +20,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CriteriaMapper {
 
-    private final ModelMapper modelMapper;
-
-//    public Criteria convertToEntity(CriteriaRequestDto criteriaRequestDto) {
-//        this.modelMapper.typeMap(CriteriaRequestDto.class, Criteria.class)
-//                .addMappings(mapper -> {
-//                            mapper.using(mappingContext -> new RangePrice((double[]) mappingContext.getSource()))
-//                                    .map(CriteriaRequestDto::getPrice, Criteria::setPrice);
-//                            mapper.using((Converter<List<ChannelRequestDto>, List<Channel>>) mappingContext ->
-//                                    mappingContext.getSource().stream().map(channelDto ->
-//                                            Channel.builder()
-//                                                    .name(channelDto.getName())
-//                                                    .desc(channelDto.getDesc())
-//                                                    .imgSrc(channelDto.getImgSrc()).build()
-//                                    ).collect(Collectors.toList()))
-//                                    .map(CriteriaRequestDto::getChannels, Criteria::setChannels);
-//                        }
-//                );
-//        return modelMapper.map(criteriaRequestDto, Criteria.class);
-//    }
-
-    public Criteria convertToEntity(CriteriaRequestDto criteriaDto){
+    public Criteria convertToEntity(CriteriaRequestDto criteriaDto) {
         List<Channel> channels = criteriaDto.getChannels().stream().map(channelDto ->
                 Channel.builder()
                         .name(channelDto.getName())
